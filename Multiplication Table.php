2026d7cd54df8153196464be452099a63db25f21
Rohit Shah <132545484+ROHIT-0P@users.noreply.github.com>
2024-03-18 06:57:04 +0530

    Update Multiplication Table.php

--- Multiplication Table.php
+++ Multiplication Table.php
@@ -2,10 +2,20 @@
 <html>
 <body>
 
+<h2>Multiplication Table</h2>
+    <form method="post">
+        <label for="num1">Enter a number:</label>
+        <input type="number" id="num1" name="num1" required><br><br>
+        <button type="submit" name="multiply">Multiply</button>
+    </form>
+
 <?php
-$n = 2;
+if (isset($_POST['multiply'])) 
+        $num1 = $_POST['num1'];
+
+$num1 = 2;
 for($i=1;$i<=10;$i++){
-$result = $n*$i;
+$result = $num1*$i;
 echo"$result <br>";
 }
 ?> 
